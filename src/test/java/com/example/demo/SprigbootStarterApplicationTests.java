630e87fb6c2a99056693f509860b85a8dd1c27de
Maduabuchi <maduabuchiokonkwo@gmail.com>
2020-05-01 07:02:46 +0100

    Application complete

---
+++ src/test/java/com/example/demo/SprigbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
